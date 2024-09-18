fae1529e27890753ad58f8b50c1cd92a72004eb7
acheronfail <acheronfail@gmail.com>
2024-09-18 19:04:39 +0930

    patch: update auto-bump script to bump package-lock.json

--- scripts/bump.ts
+++ scripts/bump.ts
@@ -1,7 +1,37 @@
 import { readFile, writeFile } from 'node:fs/promises';
 import { exec } from 'node:child_process';
+import { dirname, join } from 'node:path';
 import { promisify } from 'node:util';
 import semver, { ReleaseType } from 'semver';
+
+/**
+ * Script
+ */
+
+const versionString = env('npm_package_version');
+const version = semver.parse(versionString);
+if (!version) {
+  throw new Error(`Unable to parse version: ${versionString}`);
+}
+
+const { stdout } = await promisify(exec)('git log -1 --pretty="%B"');
+const commitMessage = stdout.trim();
+if (commitMessage.startsWith('[skip ci]')) {
+  process.exit(0);
+}
+
+const match = /^(major|minor|patch):/.exec(commitMessage);
+if (!match || !match[1]) {
+  throw new Error(`Commit message must start with 'major:', 'minor:' or 'patch:'`);
+}
+
+const newVersion = version.inc(match[1] as ReleaseType).format();
+await updatePkgFile(env('npm_package_json'));
+await updatePkgFile(join(dirname(env('npm_package_json')), 'package-lock.json'));
+
+/**
+ * Helpers
+ */
 
 function env(key: string): string {
   const val = process.env[key];
@@ -12,20 +42,8 @@ function env(key: string): string {
   return val;
 }
 
-const versionString = env('npm_package_version');
-const version = semver.parse(versionString);
-if (!version) {
-  throw new Error(`Unable to parse version: ${versionString}`);
+async function updatePkgFile(path: string) {
+  const pkgJson = JSON.parse(await readFile(path, 'utf-8'));
+  pkgJson.version = newVersion;
+  await writeFile(path, JSON.stringify(pkgJson, null, 2) + '\n');
 }
-
-const { stdout: commitMessage } = await promisify(exec)('git log -1 --pretty="%B"');
-
-const match = /^(major|minor|patch):/.exec(commitMessage.trim());
-if (!match || !match[1]) {
-  throw new Error(`Commit message must start with 'major:', 'minor:' or 'patch:'`);
-}
-
-const pkgJsonPath = env('npm_package_json');
-const pkgJson = JSON.parse(await readFile(pkgJsonPath, 'utf-8'));
-pkgJson.version = version.inc(match[1] as ReleaseType).format();
-await writeFile(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
