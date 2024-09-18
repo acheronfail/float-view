fae12900383975a99eeb51f9a5191367fefa6356
acheronfail <acheronfail@gmail.com>
2024-09-18 10:07:15 +0930

    move handling of Floaty's bad data into floaty module

--- src/lib/parse/floaty.ts
+++ src/lib/parse/floaty.ts
@@ -5,47 +5,71 @@ import { DataSource, stateCodeMap, Units, type Row } from './types';
 
 function rowsFromFloatyJson(json: ZFloatyJson): Row[] {
   const rows: Row[] = [];
-  const map = (log: ZLog, location: ZLocation): Row => ({
-    adc1: log.adc1,
-    adc2: log.adc2,
-    ah_charged: NaN,
-    ah: log.ampHours,
-    altitude: location.altitude,
-    bms_fault: NaN,
-    bms_temp: NaN,
-    bms_temp_battery: NaN,
-    current_battery: log.batteryCurrent ?? NaN,
-    current_booster: NaN,
-    current_field_weakening: NaN,
-    current_motor: log.motorCurrent ?? NaN,
-    distance: log.tripDistance,
-    duty: typeof log.dutyCycle === 'number' ? log.dutyCycle * 100 : NaN,
-    erpm: NaN,
-    gps_accuracy: location.accuracy,
-    gps_latitude: location.latitude,
-    gps_longitude: location.longitude,
-    motor_fault: log.faultCode,
-    pitch: log.pitchAngle,
-    requested_amps: NaN,
-    roll: log.rollAngle,
-    setpoint_atr: NaN,
-    setpoint_break_tilt: NaN,
-    setpoint_carve: NaN,
-    setpoint_remote: NaN,
-    setpoint_torque_tilt: NaN,
-    setpoint: NaN,
-    speed: log.speed ?? NaN,
-    state_raw: log.state,
-    state: stateCodeMap[log.state] ?? '??',
-    temp_battery: NaN,
-    temp_mosfet: log.controllerTemp,
-    temp_motor: log.motorTemp,
-    time: (log.timestamp - json.startTime) / 1000,
-    true_pitch: log.truePitchAngle,
-    voltage: log.batteryVolts ?? NaN,
-    wh_charged: NaN,
-    wh: log.wattHours,
-  });
+  // NOTE: sometimes Floaty doesn't record values, and seems to just put `null` in its logs.
+  // When it does, we backtrack until we find the last known value for it.
+  const findValue = (index: number, key: keyof ZLog): number => {
+    const current = json.logs[index]![key];
+    if (current !== null) {
+      return current;
+    }
+
+    let i = index - 1;
+    while (i > 0) {
+      const value = json.logs[i]![key];
+      if (value !== null) {
+        return value;
+      }
+
+      i--;
+    }
+
+    return 0;
+  };
+  const FLOATY_UNSUPPORTED = NaN;
+  const map = (log: ZLog, location: ZLocation, index: number): Row => {
+    const state_raw = findValue(index, 'state');
+    return {
+      adc1: findValue(index, 'adc1'),
+      adc2: findValue(index, 'adc2'),
+      ah_charged: FLOATY_UNSUPPORTED,
+      ah: findValue(index, 'ampHours'),
+      altitude: location.altitude,
+      bms_fault: FLOATY_UNSUPPORTED,
+      bms_temp: FLOATY_UNSUPPORTED,
+      bms_temp_battery: FLOATY_UNSUPPORTED,
+      current_battery: findValue(index, 'batteryCurrent'),
+      current_booster: FLOATY_UNSUPPORTED,
+      current_field_weakening: FLOATY_UNSUPPORTED,
+      current_motor: findValue(index, 'motorCurrent'),
+      distance: findValue(index, 'tripDistance'),
+      duty: findValue(index, 'dutyCycle') * 100,
+      erpm: FLOATY_UNSUPPORTED,
+      gps_accuracy: location.accuracy,
+      gps_latitude: location.latitude,
+      gps_longitude: location.longitude,
+      motor_fault: findValue(index, 'faultCode'),
+      pitch: findValue(index, 'pitchAngle'),
+      requested_amps: FLOATY_UNSUPPORTED,
+      roll: findValue(index, 'rollAngle'),
+      setpoint_atr: FLOATY_UNSUPPORTED,
+      setpoint_break_tilt: FLOATY_UNSUPPORTED,
+      setpoint_carve: FLOATY_UNSUPPORTED,
+      setpoint_remote: FLOATY_UNSUPPORTED,
+      setpoint_torque_tilt: FLOATY_UNSUPPORTED,
+      setpoint: FLOATY_UNSUPPORTED,
+      speed: findValue(index, 'speed'),
+      state_raw,
+      state: stateCodeMap[state_raw] ?? '??',
+      temp_battery: FLOATY_UNSUPPORTED,
+      temp_mosfet: findValue(index, 'controllerTemp'),
+      temp_motor: findValue(index, 'motorTemp'),
+      time: (log.timestamp - json.startTime) / 1000,
+      true_pitch: findValue(index, 'truePitchAngle'),
+      voltage: findValue(index, 'batteryVolts'),
+      wh_charged: FLOATY_UNSUPPORTED,
+      wh: findValue(index, 'wattHours'),
+    };
+  };
 
   const { logs, locations } = json;
   let locationIdx = 0;
@@ -56,7 +80,7 @@ function rowsFromFloatyJson(json: ZFloatyJson): Row[] {
       location = locations[++locationIdx]!;
     }
 
-    rows.push(map(log, location));
+    rows.push(map(log, location, i));
   }
 
   return rows;
