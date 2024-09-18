fae17424315ebd3f0c21c44be464bab6f539b90d
acheronfail <acheronfail@gmail.com>
2024-09-18 22:48:06 +0930

    patch: update package-lock.json with npm

--- scripts/bump.ts
+++ scripts/bump.ts
@@ -8,26 +8,32 @@ import semver, { ReleaseType } from 'semver';
  * Script
  */
 
+// get current version
 const versionString = env('npm_package_version');
 const version = semver.parse(versionString);
 if (!version) {
   throw new Error(`Unable to parse version: ${versionString}`);
 }
 
+// get commit message
 const { stdout } = await promisify(exec)('git log -1 --pretty="%B"');
 const commitMessage = stdout.trim();
 if (commitMessage.startsWith('[skip ci]')) {
   process.exit(0);
 }
 
+// extract commit message prefix
 const match = /^(major|minor|patch):/.exec(commitMessage);
 if (!match || !match[1]) {
   throw new Error(`Commit message must start with 'major:', 'minor:' or 'patch:'`);
 }
 
+// bump version in package.json
 const newVersion = version.inc(match[1] as ReleaseType).format();
-await updatePkgFile(env('npm_package_json'));
-await updatePkgFile(join(dirname(env('npm_package_json')), 'package-lock.json'));
+const pkgJsonPath = env('npm_package_json');
+const pkgJson = JSON.parse(await readFile(pkgJsonPath, 'utf-8'));
+pkgJson.version = newVersion;
+await writeFile(pkgJsonPath, JSON.stringify(pkgJson, null, 2) + '\n');
 
 /**
  * Helpers
@@ -40,10 +46,4 @@ function env(key: string): string {
   }
 
   return val;
-}
-
-async function updatePkgFile(path: string) {
-  const pkgJson = JSON.parse(await readFile(path, 'utf-8'));
-  pkgJson.version = newVersion;
-  await writeFile(path, JSON.stringify(pkgJson, null, 2) + '\n');
 }
