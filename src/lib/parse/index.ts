fae19571580c6af8112dffadd755a907c44d6274
acheronfail <acheronfail@gmail.com>
2024-10-01 15:59:14 +1000

    minor: initial support for allowing zipped float control rides

--- src/lib/parse/index.ts
+++ src/lib/parse/index.ts
@@ -1,3 +1,5 @@
+import * as fflate from 'fflate';
+
 import { parseFloatControlCsv } from './float-control';
 import { parseFloatyJson } from './floaty';
 import { DataSource, Units, type RowWithIndex } from './types';
@@ -19,7 +21,11 @@ export interface ParseResult {
 }
 
 export enum SupportedMimeTypes {
+  /** Float Control's CSV */
   Csv = 'text/csv',
+  /** Float Control's Zipped CSV */
+  Zip = 'application/zip',
+  /** Floaty's JSON */
   Json = 'application/json',
 }
 
@@ -28,7 +34,32 @@ export const supportedMimeTypeString = supportedMimeTypes.join(',');
 
 export async function parse(file: File): Promise<ParseResult> {
   const lowerName = file.name.toLowerCase();
-  if (file.type === 'text/csv' || lowerName.endsWith('.csv')) {
+  if (file.type === SupportedMimeTypes.Zip || lowerName.endsWith('.zip')) {
+    const fileMap = fflate.unzipSync(new Uint8Array(await file.arrayBuffer()));
+    const fileList = Object.keys(fileMap);
+    if (fileList.length !== 1) {
+      return {
+        source: DataSource.None,
+        data: [],
+        units: Units.Metric,
+        error: new ParseError('Expected a zip containing a single CSV file from Float Control!', file),
+      };
+    }
+
+    const unzippedBytes = fileMap[fileList[0]!]!;
+    const parsed = await parseFloatControlCsv(new TextDecoder().decode(unzippedBytes));
+    return {
+      source: DataSource.FloatControl,
+      data: parsed.csv.data,
+      units: parsed.units,
+      error:
+        parsed.csv.errors.length > 0
+          ? new ParseError('Failed to parse Float Control CSV properly!', parsed.csv.errors)
+          : undefined,
+    };
+  }
+
+  if (file.type === SupportedMimeTypes.Csv || lowerName.endsWith('.csv')) {
     const parsed = await parseFloatControlCsv(file);
     return {
       source: DataSource.FloatControl,
@@ -41,7 +72,7 @@ export async function parse(file: File): Promise<ParseResult> {
     };
   }
 
-  if (file.type === 'application/json' || lowerName.endsWith('.json')) {
+  if (file.type === SupportedMimeTypes.Json || lowerName.endsWith('.json')) {
     return await parseFloatyJson(file);
   }
 
